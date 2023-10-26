4b188c203648e10fe5ab134e7c96f87e27e2e57a
Johan Abde <abdejohan@gmail.com>
2023-10-26 19:53:34 +0200

    Add buttons for auth providers

--- app/register.tsx
+++ app/register.tsx
@@ -1,10 +1,31 @@
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, Button } from 'react-native-paper';
 
 export default function RegisterPage() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Register</Text>
+      <Text style={styles.title}>Register with</Text>
+      <Button
+        style={styles.button}
+        mode="contained-tonal"
+        onPress={() => console.log('clicked')}
+      >
+        <Text>Google</Text>
+      </Button>
+      <Button
+        style={styles.button}
+        mode="contained-tonal"
+        onPress={() => console.log('clicked')}
+      >
+        <Text>Apple</Text>
+      </Button>
+      <Button
+        style={styles.button}
+        mode="contained-tonal"
+        onPress={() => console.log('clicked')}
+      >
+        <Text>Facebook</Text>
+      </Button>
     </View>
   );
 }
@@ -18,5 +39,14 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 16,
+    marginTop: 100,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginTop: 20,
+    minWidth: 120,
   },
 });
